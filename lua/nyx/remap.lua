0234c24c9cdd03cb60f641763fbce14d270290bc
STNeto1 <gfreitasneto18@gmail.com>
2023-07-13 00:34:58 -0300

    yank highlight

--- lua/nyx/remap.lua
+++ lua/nyx/remap.lua
@@ -31,3 +31,16 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "g", function()
   vim.lsp.buf.format()
 end)
+
+
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
