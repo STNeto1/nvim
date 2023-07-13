00926027565039a0f03da6272ccba24280d7acae
STNeto1 <gfreitasneto18@gmail.com>
2023-07-13 00:34:22 -0300

    lualine

--- lua/nyx/lazy.lua
+++ lua/nyx/lazy.lua
@@ -55,5 +55,20 @@ require("lazy").setup({
     dependencies = {
       'nvim-tree/nvim-web-devicons'
     }
-  }
+  },
+  { 'numToStr/Comment.nvim', opts = {} },
+  {
+    -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = false,
+        -- theme = 'onedark',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
+
 })
