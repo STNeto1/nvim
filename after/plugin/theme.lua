1b81310a9141654da45a8260c5c00dc41b2323b5
STNeto1 <gfreitasneto18@gmail.com>
2023-10-28 09:24:59 -0300

    theme

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -1,6 +1,6 @@
 vim.opt.background = "dark" -- set this to dark or light
 -- Set colorscheme after options
-vim.cmd("colorscheme tokyonight-night")
+vim.cmd("colorscheme rose-pine")
 
 -- Set background to transparent
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
