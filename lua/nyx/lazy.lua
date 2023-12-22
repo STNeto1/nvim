e994c98fdbf84aaafffc6d9bca54be65871a25fc
STNeto1 <gfreitasneto18@gmail.com>
2023-12-22 20:17:48 -0300

    lualine show all filename

--- lua/nyx/lazy.lua
+++ lua/nyx/lazy.lua
@@ -63,14 +63,14 @@ require("lazy").setup({
 		-- Set lualine as statusline
 		"nvim-lualine/lualine.nvim",
 		-- See `:help lualine.txt`
-		opts = {
-			options = {
-				icons_enabled = false,
-				-- theme = "tokyonight",
-				component_separators = "|",
-				section_separators = "",
-			},
-		},
+		-- opts = {
+		-- 	options = {
+		-- 		icons_enabled = false,
+		-- 		-- theme = "tokyonight",
+		-- 		component_separators = "|",
+		-- 		section_separators = "",
+		-- 	},
+		-- },
 	},
 	{
 		"mhartington/formatter.nvim",
