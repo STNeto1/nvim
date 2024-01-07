ed12fd6555d415be4bee6c07adee0291d87533cb
STNeto1 <gfreitasneto18@gmail.com>
2024-01-07 12:11:34 -0300

    more

--- lua/nyx/lazy.lua
+++ lua/nyx/lazy.lua
@@ -12,19 +12,4 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup('nyx.plugins')
-
--- require("lazy").setup({
--- 	{ "williamboman/mason.nvim" },
--- 	{ "williamboman/mason-lspconfig.nvim" },
--- 	{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
--- 	{ "neovim/nvim-lspconfig" },
--- 	{ "hrsh7th/cmp-nvim-lsp" },
--- 	{ "hrsh7th/nvim-cmp" },
--- 	{
--- 		"L3MON4D3/LuaSnip",
--- 		-- follow latest release.
--- 		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
--- 		-- install jsregexp (optional!).
--- 		build = "make install_jsregexp",
--- 	},
+require("lazy").setup("nyx.plugins")
---
+++ lua/nyx/plugins/trouble.lua
@@ -0,0 +1,21 @@
+return {
+	"folke/trouble.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local trouble = require("trouble")
+
+		vim.keymap.set("n", "<leader>tt", function()
+			trouble.toggle()
+		end)
+
+		vim.keymap.set("n", "<leader>tn", function()
+			trouble.next({ skip_groups = true, jump = true })
+		end)
+
+		vim.keymap.set("n", "<leader>tp", function()
+			trouble.previous({ skip_groups = true, jump = true })
+		end)
+	end,
+}
