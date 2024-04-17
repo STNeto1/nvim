3a60e1ef6d69a85323275a83b43d7483cb9c4d88
STNeto1 <gfreitasneto18@gmail.com>
2024-04-17 05:16:49 -0300

    lualine theme

--- lua/nyx/plugins/lualine.lua
+++ lua/nyx/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
-				theme = "ayu_dark",
+				theme = "auto",
 				component_separators = { left = "", right = "" },
 				section_separators = { left = "", right = "" },
 				disabled_filetypes = {
