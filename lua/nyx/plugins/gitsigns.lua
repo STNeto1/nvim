b4d60635cfefa7f725ac4e813461131df68ac4c1
STNeto1 <gfreitasneto18@gmail.com>
2024-06-07 11:45:08 -0300

    signs

--- lua/nyx/plugins/gitsigns.lua
+++ lua/nyx/plugins/gitsigns.lua
@@ -3,29 +3,34 @@ return {
 	config = function()
 		require("gitsigns").setup({
 			signs = {
-				add = { text = "▎" },
-				change = { text = "▎" },
-				delete = { text = "" },
-				topdelete = { text = "" },
-				changedelete = { text = "▎" },
-				untracked = { text = "▎" },
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
 			},
 			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-			numhl = false,  -- Toggle with `:Gitsigns toggle_numhl`
+			numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
 			linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
 			word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
 			watch_gitdir = {
 				follow_files = true,
 			},
-			attach_to_untracked = true,
+			auto_attach = true,
+			attach_to_untracked = false,
 			current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
 			current_line_blame_opts = {
 				virt_text = true,
 				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
 				delay = 1000,
 				ignore_whitespace = false,
+				virt_text_priority = 100,
 			},
 			current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+			current_line_blame_formatter_opts = {
+				relative_time = false,
+			},
 			sign_priority = 6,
 			update_debounce = 100,
 			status_formatter = nil, -- Use default
@@ -38,9 +43,6 @@ return {
 				row = 0,
 				col = 1,
 			},
-			yadm = {
-				enable = false,
-			},
 		})
-	end
+	end,
 }
