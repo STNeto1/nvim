e55619bde3a40e21c6e6c32870f6b740692fe293
STNeto1 <gfreitasneto18@gmail.com>
2024-06-07 11:44:47 -0300

    trouble changes

--- lua/nyx/plugins/trouble.lua
+++ lua/nyx/plugins/trouble.lua
@@ -1,21 +1,55 @@
 return {
 	"folke/trouble.nvim",
+	opts = {
+		icons = {
+			indent = {
+				middle = " ",
+				last = " ",
+				top = " ",
+				ws = "│  ",
+			},
+		},
+		modes = {
+			diagnostics = {
+				groups = {
+					{ "filename", format = "{file_icon} {basename:Title} {count}" },
+				},
+			},
+			diagnostics_buffer = {
+				mode = "diagnostics", -- inherit from diagnostics mode
+				filter = { buf = 0 }, -- filter diagnostics to the current buffer
+			},
+		},
+	},
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		local trouble = require("trouble")
-
-		vim.keymap.set("n", "<leader>tt", function()
-			trouble.toggle()
-		end)
-
-		vim.keymap.set("n", "<leader>tn", function()
-			trouble.next({ skip_groups = true, jump = true })
-		end)
-
-		vim.keymap.set("n", "<leader>tp", function()
-			trouble.previous({ skip_groups = true, jump = true })
-		end)
+		require("trouble").setup()
+		--
+		-- vim.keymap.set("n", "<leader>tt", function()
+		-- 	trouble.toggle()
+		-- end)
+		--
+		-- vim.keymap.set("n", "<leader>tn", function()
+		-- 	trouble.next({ skip_groups = true, jump = true })
+		-- end)
+		--
+		-- vim.keymap.set("n", "<leader>tp", function()
+		-- 	trouble.previous({ skip_groups = true, jump = true })
+		-- end)
 	end,
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>tt",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>tl",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+	},
 }
