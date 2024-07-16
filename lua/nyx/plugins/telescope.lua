f658261f1f6066effc0328a09f0bcbfa12688f7f
STNeto1 <gfreitasneto18@gmail.com>
2024-07-15 21:34:57 -0300

    telescope stuff

--- lua/nyx/plugins/telescope.lua
+++ lua/nyx/plugins/telescope.lua
@@ -1,9 +1,10 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.5",
+	tag = "0.1.8",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
 		local builtin = require("telescope.builtin")
+		local theme = require("telescope.themes")
 
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[?] Find files in the project " })
 		vim.keymap.set("n", "<leader>fr", builtin.live_grep, {
@@ -12,15 +13,19 @@ return {
 		vim.keymap.set("n", "<leader>fs", builtin.grep_string, {
 			desc = "[?] Searches for the string under your cursor or selection in your current working directory",
 		})
+		vim.keymap.set("n", "<leader>F", function()
+			-- You can pass additional configuration to telescope to change theme, layout, etc.
+			builtin.current_buffer_fuzzy_find(theme.get_dropdown({
+				winblend = 10,
+				previewer = false,
+			}))
+		end, { desc = "[/] Fuzzily search in current buffer" })
 
-		-- vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
-		-- vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-		-- vim.keymap.set('n', '<leader>/', function()
-		--   -- You can pass additional configuration to telescope to change theme, layout, etc.
-		--   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-		--     winblend = 10,
-		--     previewer = false,
-		--   })
-		-- end, { desc = '[/] Fuzzily search in current buffer' })
+		vim.keymap.set("n", "<leader>fd", builtin.lsp_document_symbols, {
+			desc = "[?] Lists LSP document symbols in the current buffer",
+		})
+		vim.keymap.set("n", "<leader>fD", builtin.lsp_definitions, {
+			desc = "[?] Goto the definition of the word under the cursor, if there's only one, otherwise show all options in Telescope",
+		})
 	end,
 }
