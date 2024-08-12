823556acf74aff626ecf9471be33f52f66ee5620
STNeto1 <gfreitasneto18@gmail.com>
2024-08-12 13:21:04 -0300

    fmt stuff

--- lua/nyx/plugins/formatter.lua
+++ lua/nyx/plugins/formatter.lua
@@ -11,6 +11,17 @@ return {
 			log_level = vim.log.levels.WARN,
 			-- All formatter configurations are opt-in
 			filetype = {
+				sql = {
+					function()
+						return {
+							exe = "sleek",
+							args = {
+								util.escape_path(util.get_current_buffer_file_path()),
+							},
+							stdin = false,
+						}
+					end,
+				},
 				c = {
 					require("formatter.filetypes.c").clangformat,
 				},
@@ -35,14 +46,6 @@ return {
 				astro = {
 					require("formatter.defaults.biome"),
 				},
-				javascriptreact = {
-					require("formatter.defaults.biome"),
-				},
-				svelte = {
-					function()
-						vim.lsp.buf.format({ async = true })
-					end,
-				},
 				go = {
 					require("formatter.filetypes.go").goimports,
 				},
@@ -52,57 +55,28 @@ return {
 				graphql = {
 					require("formatter.filetypes.graphql").biome,
 				},
-				python = {
-					require("formatter.filetypes.python").ruff,
-				},
-				elixir = {
-					require("formatter.filetypes.elixir").mixformat,
-				},
 				yaml = {
 					require("formatter.filetypes.yaml").yamlfmt,
 				},
-				heex = {
-					function()
-						return {
-							exe = "mix",
-							args = {
-								"format",
-							},
-							stdin = false,
-						}
-					end,
-				},
-				-- ocaml = {
-				-- 	function()
-				-- 		return {
-				-- 			exe = "ocamlformat",
-				-- 			args = {
-				-- 				"--enable-outside-detected-project",
-				-- 				util.escape_path(util.get_current_buffer_file_path()),
-				-- 			},
-				-- 			stdin = true,
-				-- 		}
-				-- 	end,
-				-- },
 
 				-- Use the special "*" filetype for defining formatter configurations on
 				-- any filetype
-				["*"] = {
-					-- "formatter.filetypes.any" defines default configurations for any
-					-- filetype
-					-- require("formatter.filetypes.any").remove_trailing_whitespace,
-
-					-- -- code to use default lsp formatter if nothing matched before
-					-- -- its not good because it lags a lot
-					function()
-						-- Ignore already configured types.
-						local defined_types = require("formatter.config").values.filetype
-						if defined_types[vim.bo.filetype] ~= nil or vim.bo.filetype == "make" then
-							return nil
-						end
-						vim.lsp.buf.format({ async = true })
-					end,
-				},
+				-- ["*"] = {
+				-- 	-- "formatter.filetypes.any" defines default configurations for any
+				-- 	-- filetype
+				-- 	-- require("formatter.filetypes.any").remove_trailing_whitespace,
+				--
+				-- 	-- -- code to use default lsp formatter if nothing matched before
+				-- 	-- -- its not good because it lags a lot
+				-- 	function()
+				-- 		-- Ignore already configured types.
+				-- 		local defined_types = require("formatter.config").values.filetype
+				-- 		if defined_types[vim.bo.filetype] ~= nil or vim.bo.filetype == "make" then
+				-- 			return nil
+				-- 		end
+				-- 		vim.lsp.buf.format({ async = true })
+				-- 	end,
+				-- },
 			},
 		})
 
