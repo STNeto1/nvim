cb7abdf1a169350bd1434cf52230f08dcb241674
STNeto1 <gfreitasneto18@gmail.com>
2025-03-08 07:58:50 -0300

    colorscheme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -3,8 +3,18 @@ return {
     'rose-pine/neovim',
     lazy = false,
     priority = 1000,
+    -- config = function()
+    --   vim.cmd.colorscheme 'rose-pine'
+    --   vim.cmd.hi 'Comment gui=none'
+    -- end,
+  },
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
     config = function()
-      vim.cmd.colorscheme 'rose-pine'
+      vim.cmd.colorscheme 'tokyonight'
       vim.cmd.hi 'Comment gui=none'
     end,
   },
