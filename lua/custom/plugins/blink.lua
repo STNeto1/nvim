4b8aec06593df32867c02e62468752f266e072dd
STNeto1 <gfreitasneto18@gmail.com>
2025-04-26 08:08:33 -0300

    blink kinda

--- lua/custom/plugins/blink.lua
+++ lua/custom/plugins/blink.lua
@@ -38,12 +38,13 @@ return {
     },
 
     -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
+    completion = { documentation = { auto_show = true } },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      -- default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = { 'lsp' },
     },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
