0a13d403e55fef50d50210a4b72dffbf98d05e34
STNeto1 <gfreitasneto18@gmail.com>
2026-03-15 21:20:18 -0300

    batch 2

--- init.lua
+++ init.lua
@@ -155,6 +155,12 @@ vim.pack.add({
 	"https://github.com/mason-org/mason.nvim",
 	"https://github.com/loctvl842/monokai-pro.nvim",
 	"https://github.com/nvim-lualine/lualine.nvim",
+
+	"https://github.com/neovim/nvim-lspconfig",
+	"https://github.com/folke/lazydev.nvim",
+	"https://github.com/b0o/SchemaStore.nvim",
+	"https://github.com/nvim-telescope/telescope.nvim",
+	"https://github.com/nvim-lua/plenary.nvim",
 })
 
 --- === COLORSCHEME ===
@@ -274,3 +280,53 @@ require("conform").setup({
 vim.keymap.set("n", "<leader>f", function()
 	require("conform").format({ async = true, lsp_format = "fallback" })
 end, { desc = "[F]ormat buffer" })
+
+-- === LSP ===
+vim.cmd("packadd lazydev.nvim")
+require("lazydev.config").setup({
+	library = {
+		{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+	},
+})
+
+for _, svr_name in ipairs({
+	"gopls",
+	"tsgo",
+	"rust_analyzer",
+	"oxlint",
+	"stylua",
+}) do
+	vim.lsp.enable(svr_name)
+
+	-- vim.lsp.config(svr_name, {
+	--   capabilities = require('blink.cmp').get_lsp_capabilities(),
+	-- })
+end
+
+vim.cmd("packadd telescope.nvim")
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local bufnr = args.buf
+
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client:supports_method("textDocument/completion") then
+			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = false })
+		end
+		if client:supports_method("textDocument/inlayHint") then
+			vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+		end
+
+		local map = function(keys, func, desc, mode)
+			mode = mode or "n"
+			vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
+		end
+
+		local builtin = require("telescope.builtin")
+		vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
+		map("gd", builtin.lsp_definitions, "[G]oto [D]efinition")
+		map("gr", builtin.lsp_references, "[G]oto [R]eferences")
+		map("<F2>", vim.lsp.buf.rename, "[R]e[n]ame")
+		map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
+		map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+	end,
+})
